6e0f5942e761fdbac605c90192998cc99de6f4f1
jinghewang <jinghewang@163.com>
2017-11-07 23:24:04 +0800

    add config key

--- src/config/web.php
+++ src/config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'r6ajOGWbKsDnIybrzMrw-5sY_do8tca_',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
